522fa2550e9874d425bc65e7e04852417e71b1f1
Prof254852 <profgeorgemucuka026@gmail.com>
2024-12-05 12:02:19 +0300

    Add files via upload

---
+++ CAT2_Q2.C_i_and_ii[1].c
@@ -0,0 +1,10 @@
+// An array is a data structure that stores a fixed 
+// size sequence of elements all of the same time
+#include <stdio.h>
+
+int main() {
+    // Declaring and initializing the 2D array
+    int scores[2][2] = {{50, 65}, {90, 80}};
+    
+    return 0;
+}
