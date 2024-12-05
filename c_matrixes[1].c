d2532dab3de2e9a3fb9cf49078c108fda3f39d44
Prof254852 <profgeorgemucuka026@gmail.com>
2024-12-05 12:11:37 +0300

    Add files via upload

---
+++ c_matrixes[1].c
@@ -0,0 +1,14 @@
+//c matrixes 
+#include<stdio.h> 
+int main () {
+int i,j;
+int marks[2]3]= {
+{40,10,20};
+{50,80,70};
+};
+for(I=0;i<2;I++);
+for(j=0;j<3;j++){ // rows
+printf("marks[%d][%d]=%d\n",I,j, marks[i][j]);
+)
+return 0;
+}
